fb325a98351b191152bce0be628840d094155dab
szabo003 <98863648+szabo003@users.noreply.github.com>
2022-12-16 01:40:16 -0600

    Add files via upload

---
+++ Project 2/index/js/lesson.js
@@ -0,0 +1,45 @@
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+const box = document.getElementById('box');
+
+// 👇️ Change text color on mouseover
+box.addEventListener('mouseover', function handleMouseOver() {
+  box.style.color = 'red';
+});
+
+// 👇️ Change text color back on mouseout
+box.addEventListener('mouseout', function handleMouseOut() {
+  box.style.color = 'black';
+});
+
+</script>
